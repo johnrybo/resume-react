dbcd46e0da68650c3d5d6220905ff679a5981ffb
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-02-20 22:23:32 +0100

    cv

--- src/Intro.js
+++ src/Intro.js
@@ -5,6 +5,7 @@ function Intro() {
         <div className="Intro">
             <h3><a href="https://github.com/johnrybo">GitHub</a></h3>
             <h3><a href="https://www.linkedin.com/in/johnrybo/">LinkedIn</a></h3>
+            <h3><a href="https://www.johnrybo.com">CV</a></h3>
         </div>
       );
 }
