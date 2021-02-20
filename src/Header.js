5629b4652b5eace11a16832b4a4d9ae331ec93bd
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-02-20 22:15:11 +0100

    diverse

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div className="Header">
             <h1>John Rybo</h1>
+            <h2>Frontend-utvecklare</h2>
         </div>
       );
 }
--- src/Intro.js
+++ src/Intro.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Intro() {
     return (
         <div className="Intro">
-            <h2>Om mig</h2>
+            <h3><a href="https://www.johnrybo.se">GitHub</a></h3>
+            <h3><a href="https://www.johnrybo.se">LinkedIn</a></h3>
         </div>
       );
 }
