38eca9b9f32044b78262d47fc8a996f57fe42986
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-02-20 19:08:29 +0100

    calendar

--- src/Main.js
+++ src/Main.js
@@ -5,6 +5,7 @@ import djupvik from './images/djupvik-480w.png'
 import gokart from './images/gokart-480w.png'
 import lab1 from './images/lab-1-480w.png'
 import rainbowman from './images/rainbowman-480w.png'
+import calendar from './images/calendar-480w.png'
 
 function Main() {
     return (
@@ -13,6 +14,7 @@ function Main() {
             <Project title="Gokart" img={gokart} alt="Gokart" description="Hemsida"/>
             <Project title="JS-Lab-1" img={lab1} alt="Lab 1" description="Spel" />
             <Project title="Rainbow Man" img={rainbowman} alt="Rainbow Man" description="Spel" />
+            <Project title="Calendar" img={calendar} alt="Calendar" description="Kalender" />
         </div>
       );
 }
