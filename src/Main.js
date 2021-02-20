6b011cd8c744b3b4bb1a7fd9b7870360b9b22e8b
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-02-20 19:16:44 +0100

    diverse

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Project from './Project'
 
-import djupvik from './images/djupvik-480w.png'
+import weatherWidget from './images/weatherwidget-480w.png'
 import gokart from './images/gokart-480w.png'
 import lab1 from './images/lab-1-480w.png'
 import rainbowman from './images/rainbowman-480w.png'
@@ -10,11 +10,11 @@ import calendar from './images/calendar-480w.png'
 function Main() {
     return (
         <div className="Main">
-            <Project title="Djupviks hamn" img={djupvik} alt="Djupviks hamn" description="Spel" />
-            <Project title="Gokart" img={gokart} alt="Gokart" description="Hemsida"/>
-            <Project title="JS-Lab-1" img={lab1} alt="Lab 1" description="Spel" />
+            <Project title="Weather Widget" img={weatherWidget} alt="Weather Widget" description="Hemsida" />
             <Project title="Rainbow Man" img={rainbowman} alt="Rainbow Man" description="Spel" />
             <Project title="Calendar" img={calendar} alt="Calendar" description="Kalender" />
+            <Project title="JS-Lab-1" img={lab1} alt="Lab 1" description="Spel" />
+            <Project title="Gokart" img={gokart} alt="Gokart" description="Hemsida"/>
         </div>
       );
 }
