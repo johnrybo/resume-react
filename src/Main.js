079cad41fcbdb3c90fe7993ca33c1119666d335a
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-02-20 18:10:36 +0100

    images

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,7 @@ import Project from './Project'
 import djupvik from './images/djupvik-480w.png'
 import gokart from './images/gokart-480w.png'
 import lab1 from './images/lab-1-480w.png'
+import rainbowman from './images/rainbowman-480w.png'
 
 function Main() {
     return (
@@ -11,6 +12,7 @@ function Main() {
             <Project title="Djupviks hamn" img={djupvik} alt="Djupviks hamn" description="Spel" />
             <Project title="Gokart" img={gokart} alt="Gokart" description="Hemsida"/>
             <Project title="JS-Lab-1" img={lab1} alt="Lab 1" description="Spel" />
+            <Project title="Rainbow Man" img={rainbowman} alt="Rainbow Man" description="Spel" />
         </div>
       );
 }
--- src/Project.js
+++ src/Project.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import './App.css';
 
 function Project(props) {
     return (
         <div className="Project">
-            <img src={props.img} alt={props.alt}></img>
+            <img className="projectImage" src={props.img} alt={props.alt}></img>
             <h2>{props.title}</h2>
             <p>{props.description}</p>
         </div>
