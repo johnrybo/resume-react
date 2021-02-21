5b5ab40a49b2c6b7c21549d5277010bfbff7ae3a
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-02-21 13:51:25 +0100

    animationer

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,8 @@ function Header() {
         <div className="Header">
             <h1>John Rybo</h1>
             <h2>Frontend-utvecklare</h2>
+            <p>Scrolla vidare för att se mina projekt</p>
+            <i class="fas fa-arrow-down"></i>
         </div>
       );
 }
