ef016b18ca4168f585edbfc5f9d1847277ed00e0
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-02-21 14:20:46 +0100

    styling

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import profilePic from './images/foto-480w.png'
 
 function Header() {
     return (
         <div className="Header">
+            <img className="profilePic" src={profilePic} alt=''></img>
             <h1>John Rybo</h1>
             <h2>Frontend-utvecklare</h2>
             <p>Scrolla vidare för att se mina projekt</p>
             <i class="fas fa-arrow-down"></i>
         </div>
-      );
+    );
 }
 
 export default Header;
