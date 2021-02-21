4d1b6ebb30637631a0d79d8976a57481276805b2
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-02-21 14:57:37 +0100

    ikoner

--- src/Intro.js
+++ src/Intro.js
@@ -3,11 +3,21 @@ import React from 'react';
 function Intro() {
     return (
         <div className="Intro">
-            <h3><a href="https://github.com/johnrybo">GitHub</a></h3>
-            <h3><a href="https://www.linkedin.com/in/johnrybo/">LinkedIn</a></h3>
-            <h3><a href="https://www.johnrybo.com">CV</a></h3>
+            <h3>
+                <a href="https://github.com/johnrybo">
+                    <i class="fab fa-github"></i>
+                </a>
+            </h3>
+            <h3>
+                <a href="https://www.linkedin.com/in/johnrybo/">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+            </h3>
+            <h3>
+                <a href="https://www.johnrybo.com">CV</a>
+            </h3>
         </div>
-      );
+    );
 }
 
 export default Intro;
