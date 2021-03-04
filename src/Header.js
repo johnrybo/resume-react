276f7b3cc9ce9530612b181cc6cde0f2f5df7fad
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-03-04 22:48:50 +0100

    färger osv

--- src/Header.js
+++ src/Header.js
@@ -4,22 +4,24 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div className="Header">
-            <h3>
-                <Link to="/" >Hem</Link>
-            </h3>
-            <h3>
-                <Link to="/cv" >CV</Link>
-            </h3>
-            <h3>
-                <a href="https://github.com/johnrybo">
-                    <i class="fab fa-github"></i>
-                </a>
-            </h3>
-            <h3>
-                <a href="https://www.linkedin.com/in/johnrybo/">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-            </h3>
+            <div className="HeaderContainer">
+                <h3>
+                    <Link to="/" >Hem</Link>
+                </h3>
+                <h3>
+                    <Link to="/cv" >CV</Link>
+                </h3>
+                <h3>
+                    <a href="https://github.com/johnrybo">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </h3>
+                <h3>
+                    <a href="https://www.linkedin.com/in/johnrybo/">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </h3>
+            </div>
         </div>
     );
 }
--- src/Presentation.js
+++ src/Presentation.js
@@ -4,9 +4,11 @@ import profilePic from './images/foto-480w.png'
 function Presentation() {
     return (
         <div className="Presentation">
-            <img className="profilePic" src={profilePic} alt=''></img>
-            <h1>John Rybo</h1>
-            <h2>Frontend-utvecklare</h2>
+            <div className="Container">
+                <img className="profilePic" src={profilePic} alt=''></img>
+                <h1>John Rybo</h1>
+                <h2>Frontend-utvecklare</h2>
+            </div>
             <p>Scrolla vidare för att se mina projekt</p>
             <i class="fas fa-arrow-down"></i>
         </div>
