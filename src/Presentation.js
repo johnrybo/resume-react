5e1f1e56dd1fc2095dae27fa4052efc7612f4476
John Rybo <68517441+johnrybo@users.noreply.github.com>
2021-03-04 23:46:05 +0100

    test

--- src/Presentation.js
+++ src/Presentation.js
@@ -4,8 +4,8 @@ import profilePic from './images/foto-480w.png'
 function Presentation() {
     return (
         <div className="Presentation">
+            <img className="profilePic" src={profilePic} alt=''></img>
             <div className="Container">
-                <img className="profilePic" src={profilePic} alt=''></img>
                 <h1>John Rybo</h1>
                 <h2>Frontend-utvecklare</h2>
             </div>
