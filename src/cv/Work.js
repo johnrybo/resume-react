3d5ebc8bf3635ad32df7c6515c16a5150ed632ae
John <john.rybo@gmail.com>
2021-03-18 22:37:04 +0100

    färger

--- src/cv/Work.js
+++ src/cv/Work.js
@@ -6,7 +6,7 @@ export default function Work() {
             <h2 class="resumeH2">Arbetslivserfarenhet</h2>
 
             <h3 className="resumeH3">PostNord</h3>
-            <h4 className="resumeH4">2010 - 2020</h4>
+            <h4 className="resumeH4">2010 -</h4>
             <h4 className="resumeH4">Brevbärare</h4>
 
             <h3 className="resumeH3">Göteborgs universitet</h3>
