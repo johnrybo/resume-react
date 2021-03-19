db0238f7e4dd9eaef2f8f38c3caa3db79f0987ab
John <john.rybo@gmail.com>
2021-03-20 00:46:02 +0100

    fix

--- src/cv/Cv.js
+++ src/cv/Cv.js
@@ -1,25 +1,15 @@
-import React from 'react'
+import React from "react";
 
-import Education from './Education';
-import Work from './Work';
-import About from './About'
+import Education from "./Education";
+import Work from "./Work";
+import About from "./About";
 
 export default function Cv() {
-
-    const ready = true;
-
-    if (ready){
-        return (
-            <div className="Cv">
-                <Education />
-                <Work />
-                <About />
-            </div>
-        );
-    } else {
-        return(
-            <div className="Brb">Sidan är under konstruktion</div>
-        )
-    }
-    
+  return (
+    <div className="Cv">
+      <Education />
+      <Work />
+      <About />
+    </div>
+  );
 }
