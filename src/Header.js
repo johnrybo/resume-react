1d152464dea892d300bf5cd04900ba7c75b36862
John <john.rybo@gmail.com>
2021-03-20 17:45:02 +0100

    diverse

--- src/Header.js
+++ src/Header.js
@@ -2,29 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-import { FaLinkedin, FaGithub } from "react-icons/fa";
-
 function Header() {
   return (
     <div className="Header">
       <div className="HeaderContainer">
-        <motion.span whileHover={{ scale: 1.2 }}>
+        <motion.a whileHover={{ scale: 1.2 }}>
           <Link to="/">Hem</Link>
-        </motion.span>
-        <motion.span whileHover={{ scale: 1.2 }}>
-          <Link to="/cv">CV</Link>
-        </motion.span>
-        <motion.a
-          href="https://github.com/johnrybo"
-          whileHover={{ scale: 1.2 }}
-        >
-          <FaGithub />
         </motion.a>
-        <motion.a
-          href="https://www.linkedin.com/in/johnrybo/"
-          whileHover={{ scale: 1.2 }}
-        >
-          <FaLinkedin />
+        <motion.a whileHover={{ scale: 1.2 }}>
+          <Link to="/cv">CV</Link>
         </motion.a>
       </div>
     </div>
--- src/home/Presentation.js
+++ src/home/Presentation.js
@@ -3,6 +3,7 @@ import profilePic from "../images/foto-480w.png";
 import { motion } from "framer-motion";
 
 import { AiOutlineArrowDown } from "react-icons/ai";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Presentation() {
   return (
@@ -29,6 +30,26 @@ function Presentation() {
           >
             FRONTEND-UTVECKLARE
           </motion.h2>
+
+          <div className="PresentationLinks">
+            <motion.a
+              href="https://github.com/johnrybo"
+              whileHover={{ scale: 1.2 }}
+              initial={{ y: 50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1, transition: { duration: 2 } }}
+            >
+              <FaGithub />
+            </motion.a>
+
+            <motion.a
+              href="https://www.linkedin.com/in/johnrybo/"
+              whileHover={{ scale: 1.2 }}
+              initial={{ y: 50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1, transition: { duration: 2 } }}
+            >
+              <FaLinkedin />
+            </motion.a>
+          </div>
         </div>
       </div>
       <p>Scrolla vidare för att se mina projekt</p>
