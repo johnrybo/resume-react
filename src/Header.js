43347086bfb8a1d3f9bad77e9c141f63b660748b
John <john.rybo@gmail.com>
2021-03-21 16:48:56 +0100

    header

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 function Header(props) {
   return (
     <nav className="Header">
-        <motion.a whileHover={{ scale: 1.2 }}>
+      <nav className="LeftHeaderContainer">
+        <motion.a
+          whileHover={{ scale: 1.2 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 2 } }}
+        >
           <Link to={props.link}>{props.title}</Link>
         </motion.a>
+      </nav>
+      <nav className="RightHeaderContainer">
+        <motion.a
+          href="https://github.com/johnrybo"
+          whileHover={{ scale: 1.2 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 2 } }}
+        >
+          <FaGithub />
+        </motion.a>
+
+        <motion.a
+          href="https://www.linkedin.com/in/johnrybo/"
+          whileHover={{ scale: 1.2 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 2 } }}
+        >
+          <FaLinkedin />
+        </motion.a>
+      </nav>
     </nav>
   );
 }
