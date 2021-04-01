24cd76ed78fd1bf7d9af5906b3059879d408a2ba
John <john.rybo@gmail.com>
2021-04-01 13:40:03 +0200

    printyprint

--- src/projects.js
+++ src/projects.js
@@ -2,6 +2,7 @@ import weatherWidget from "./images/weatherwidget-480w.png";
 import lab1 from "./images/lab-1-800w.png";
 import rainbowman from "./images/rainbowman.png";
 import calendar from "./images/calendar.png";
+import printyprint from './images/printyprint.png'
 
 export const projects = [
   {
@@ -19,16 +20,23 @@ export const projects = [
     demo: "https://jmlweather.netlify.app/",
   },
   {
+    title: "PrintyPrint",
+    image: printyprint,
+    backgroundColor: "#2D354D",
+    gitHub: "https://github.com/miaherman/mini-projekt-webshop",
+    demo: "https://printyprint-webshop.netlify.app/",
+  },
+  {
     title: "Calendar",
     image: calendar,
-    backgroundColor: "#2D354D",
+    backgroundColor: "#343D59",
     gitHub: "https://github.com/johnrybo/Kalender",
     demo: "https://johnrybo.github.io/Kalender/",
   },
   {
     title: "Text Adventure",
     image: lab1,
-    backgroundColor: "#343D59",
+    backgroundColor: "#2D354D",
     gitHub: "https://github.com/johnrybo/Textbaserad-interaktiv-upplevelse",
     demo: "https://johnrybo.github.io/Textbaserad-interaktiv-upplevelse",
   },
