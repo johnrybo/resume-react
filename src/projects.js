ddf56e1e199f6a27b8af74b59446cb736947a4f0
John <john.rybo@gmail.com>
2021-05-14 11:49:15 +0200

    fix

--- src/projects.js
+++ src/projects.js
@@ -19,24 +19,24 @@ export const projects = [
     gitHub: "https://github.com/johnrybo/miniprojekt-react-1",
     demo: "https://jmlweather.netlify.app/",
   },
-  {
-    title: "Chat App",
-    image: chatapp,
-    backgroundColor: "#2D354D",
-    gitHub: "https://github.com/johnrybo/lab-3-socket-chat",
-    demo: "https://chattt-appp.herokuapp.com/",
-  },
+  // {
+  //   title: "Chat App",
+  //   image: chatapp,
+  //   backgroundColor: "#2D354D",
+  //   gitHub: "https://github.com/johnrybo/lab-3-socket-chat",
+  //   demo: "https://chattt-appp.herokuapp.com/",
+  // },
   {
     title: "PrintyPrint",
     image: printyprint,
-    backgroundColor: "#343D59",
+    backgroundColor: "#2D354D",
     gitHub: "https://github.com/miaherman/mini-projekt-webshop",
     demo: "https://printyprint-webshop.netlify.app/",
   },
   {
     title: "Calendar",
     image: calendar,
-    backgroundColor: "#2D354D",
+    backgroundColor: "#343D59",
     gitHub: "https://github.com/johnrybo/Kalender",
     demo: "https://johnrybo.github.io/Kalender/",
   }
